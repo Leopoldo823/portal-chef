2facb5eef029b658eb5c0977a3a2a91c94a32f2e
Leopoldo <isishadai77@gmail.com>
2026-03-26 12:58:50 -0600

    fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Portal Chef funcionando 🚀</h1>
+    <div style={{padding: 40}}>
+      <h1>🔥 Portal Chef funcionando</h1>
+      <p>Ya no hay errores 🚀</p>
     </div>
   );
 }
